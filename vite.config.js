3ced23a55b267d420c57f9f552985ec03186d669
Stephen <127278005+slmov215@users.noreply.github.com>
2023-08-14 19:38:34 -0400

    attempted to fix images paths

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,41 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// // import { viteStaticCopy } from 'vite-plugin-static-copy';
+// import path from "path";
+// import { resolve } from "path"
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(),
+//     // viteStaticCopy({
+//     //   targets: [
+//     //     {
+//     //       src: path.resolve(__dirname, './public/img') + '/[!.]*', 
+//     //       dest: './', 
+//     //     },
+//     //   ],
+//     // })
+//   ],
+//   resolve: {
+//     alias: [{ 
+//       find: "@server", 
+//       replacement: resolve(__dirname, './src/server/') 
+//     }]
+//   },
+//   server: {
+//     port: 3000,
+//     open: true
+//   }
+// })
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-import path from 'path';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: path.resolve(__dirname, './public/img') + '/[!.]*', 
-          dest: './', 
-        },
-      ],
-    })
-  ],
-  server: {
-    port: 3000,
-    open: true
-  }
-})
+  plugins: [react({
+    jsxRuntime: 'classic' // Add this line
+  }), viteTsconfigPaths(), svgrPlugin()],
+});

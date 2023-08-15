e2f05c444a31924b2fc6be101245893da82162b3
Stephen <127278005+slmov215@users.noreply.github.com>
2023-08-14 20:40:02 -0400

    fixed images path

--- src/components/pages/Projects/data/projects.jsx
+++ src/components/pages/Projects/data/projects.jsx
@@ -1,49 +1,49 @@
-// import WeatherApp from "../../assets/images/weather-dashboard-screen-shot.jpeg";
-// import project1 from "../../assets/images/project-1.jpeg";
-// import employeeTracker from "../../assets/images/employee-tracker-demo.jpeg";
-// import techBlog from "../../assets/images/my-tech-blog.jpeg";
-// import project2 from "../../assets/images/project-2.jpeg";
-// import pwa from "../../assets/images/pwa-text-editor.jpeg";
+import WeatherApp from "../../../../assets/images/weather-dashboard-screen-shot.gif";
+import project1 from "../../../../assets/images/project-1.png";
+import employeeTracker from "../../../../assets/images/employee-tracker-demo.gif";
+import techBlog from "../../../../assets/images/my-tech-blog.jpeg";
+import project2 from "../../../../assets/images/project-2.png";
+import pwa from "../../../../assets/images/pwa-text-editor.gif";
 
 const projectsData = [
   {
     title: 'Weather App',
-    image: '../assets/img/weather-dashboard-screen-shot.jpeg',
+    image: WeatherApp,
     description: 'Description of Weather App',
     link: 'https://slmov215.github.io/weather-dashboard/',
     gitHub: 'https://github.com/slmov215/weather-dashboard',
   },
     {
       title: 'Project 1',
-      image: '../assets/img/project-1.jpeg',
+      image: project1,
       description: 'Description of project 1',
       link: 'https://paulnxn1.github.io/food_Focus/Vegan/veganIndex.html',
       gitHub: 'https://github.com/PaulNXN1/food_Focus',
     },
     {
       title: 'Employee Tracker',
-      image: '../assets/img/employee-tracker-demo.jpeg',
+      image: employeeTracker,
       description: 'Description of Employee tracker',
       link: 'https://drive.google.com/file/d/1r7yIBk7enMAX2aziXzwKoqegtvwp2hHs/view',
       gitHub: 'https://github.com/slmov215/employee-traker-sql',
     },
     {
       title: 'My Tech Blog',
-      image: '../assets/img/my-tech-blog.jpeg',
+      image: techBlog,
       description: 'Description of My Tech Blog',
       link: 'https://my-mvc-tech-blog-slmov-2a0483569b53.herokuapp.com/',
       gitHub: 'https://github.com/slmov215/mvc-tech-blog',
     },
     {
       title: 'Project 2',
-      image: '../assets/img/project-2.jpeg',
+      image: project2,
       description: 'Description of project 2',
       link: 'https://bookbusta-950538aa625f.herokuapp.com/',
       gitHub: 'https://github.com/Nneifem/bookbusta',
     },
     {
       title: 'PWA - JATE',
-      image: '../assets/img/pwa-text-editor.jpeg',
+      image: pwa,
       description: 'Description of project 2',
       link: 'https://my-pwa-text-editor-week-19-9be97db16d1d.herokuapp.com/',
       gitHub: 'https://github.com/slmov215/pwa-text-editor',
